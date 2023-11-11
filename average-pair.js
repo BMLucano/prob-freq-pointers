2253678f4788dfd7cf126f720b4c184d2601e9d4
Brandie Lucano <blucano.dev@gmail.com>
2023-11-10 16:03:34 -0800

    working functions

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,26 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
+"use strict";
+
+/**Accepts a sorted array and a number.
+ * Checks if there is a pair of numbers in the array whose average equals given target average.
+ * Returns true if yes, false if no.
+ *
+ * [3, 3, 6, 12, 19], 8 -> false
+ * [1, 2, 3], 2 -> true
+ */
+function averagePair(nums, targetAvg) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while(left < right){
+    let avg = (nums[left] + nums[right]) / 2;
+    if(avg === targetAvg){
+      return true;
+    }else if(avg > targetAvg){
+      right--;
+    }else{
+      left++;
+    }
+  }
+  return false;
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,34 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+"use strict";
+
+/**Takes input of string word and a string of letters.
+ * Checks if a word can be built with letters given.
+ * Returns true if yes, false if no.
+ * ('aabb', 'bcabad') -> true
+ * ('aa', 'abc') -> false
+ */
+function canConstructWord(word, letters) {
+  const letterFreq = getFrequencyCounter(letters);
+
+  for(let char of word){
+    if(char in letterFreq && letterFreq[char] !== 0){
+      letterFreq[char] -= 1;
+    }else{
+      return false;
+    }
+  }
+  return true;
+}
+
+/** Accepts a string.
+ * Creates an object of letters and their frequency.
+ * Returns frequency object.
+*/
+function getFrequencyCounter (items){
+  const freqs = {};
+
+  for(const item of items){
+    const curr = freqs[item] || 0;
+    freqs[item] = curr + 1;
+  }
+  return freqs;
 }
