9af50b9890563d0c4a5f5b41c1000644c46844cd
Brandie Lucano <blucano.dev@gmail.com>
2023-11-10 16:14:37 -0800

    made small changes to freq counter

--- can-construct-word.js
+++ can-construct-word.js
@@ -27,8 +27,8 @@ function getFrequencyCounter (items){
   const freqs = {};
 
   for(const item of items){
-    const curr = freqs[item] || 0;
-    freqs[item] = curr + 1;
+    // const curr = freqs[item] || 0;
+    freqs[item] = ++freqs[item] || 1;
   }
   return freqs;
 }
